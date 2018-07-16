5eb231841f7751ff5b9a50ce5c67af4b61ae5c42
DavidPyn27 <popayan159@gmail.com>
2018-07-16 10:03:37 -0500

    commit20

--- EnsayoMapa/app/src/main/java/com/aprendiz/ragp/ensayomapa/MapsActivity.java
+++ EnsayoMapa/app/src/main/java/com/aprendiz/ragp/ensayomapa/MapsActivity.java
@@ -42,5 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng nueva = new LatLng(-35, 161);
+        mMap.addMarker(new MarkerOptions().position(nueva).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(nueva));
+
     }
 }
